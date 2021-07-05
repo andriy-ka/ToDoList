638d7601dfb979e0f18cd20a9cf05d0646b4f3ad
Andrii Kachur <andrii_kachur@epam.com>
2021-07-05 14:57:32 +0300

    Add log4j, add log file

--- src/main/java/andriy/todolist/aspect/AspectLog.java
+++ src/main/java/andriy/todolist/aspect/AspectLog.java
@@ -5,13 +5,23 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
+import org.apache.log4j.Logger;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Component
 @Aspect
 public class AspectLog {
 
+    final Logger logger = Logger.getLogger(AspectLog.class);
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    LocalDateTime now = LocalDateTime.now();
+
     @Before("execution(* andriy.todolist.service.*.*(..))")
     public void beforeServiceMethodInvocation(JoinPoint jp) {
-        System.out.println("Invocation of method " + jp.getSignature());
+        logger.debug(dtf.format(now) + " Invocation of method " + jp.getSignature());
+//        System.out.println(dtf.format(now) + " Invocation of method " + jp.getSignature());
     }
 
 }
