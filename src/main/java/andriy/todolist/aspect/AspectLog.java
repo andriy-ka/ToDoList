f22a654e5eaade13677243f295133813286ac39d
Andrii Kachur <andrii_kachur@epam.com>
2021-07-07 17:11:33 +0300

    Add log4j2

--- src/main/java/andriy/todolist/aspect/AspectLog.java
+++ src/main/java/andriy/todolist/aspect/AspectLog.java
@@ -1,5 +1,7 @@
 package andriy.todolist.aspect;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -9,9 +11,11 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class AspectLog {
 
+    private static final Logger LOG = LogManager.getLogger(AspectLog.class);
+
     @Before("execution(* andriy.todolist.service.*.*(..))")
     public void beforeServiceMethodInvocation(JoinPoint jp) {
-        System.out.println("Invocation of method " + jp.getSignature());
+        LOG.info("Invocation of method " + jp.getSignature());
     }
 
 }
